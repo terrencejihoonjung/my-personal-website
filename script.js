14a975897fadd898b8f529eb1cbccda33a92ec6b
--replace-all <terrencejung928@gmail.com>
2022-05-11 18:40:38 -0700

    Added script.js and modified css

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create an h2 element to the body element in DOM
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+
+// Include code to add this h2 element to the body element in the DOM
+document.querySelector("body").appendChild(h2);
+
